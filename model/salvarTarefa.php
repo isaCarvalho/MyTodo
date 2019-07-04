7831b4b8ac7fe279fd48ea9bea9c1b3ca3cd13e9
isaCarvalho <isabelacarvalhocf@hotmail.com>
2019-07-04 12:51:11 -0300

    Inserindo tarefas com status

--- model/salvarTarefa.php
+++ model/salvarTarefa.php
@@ -9,8 +9,10 @@ function salvarTarefa()
     session_start();
     
     $id_usuario = $_SESSION['login'];
-    
-    Query::insert("tarefas", "nome, inicio, fim, data, id_usuario, id_recorrencia", "'$nome', '$inicio', '$fim', '$data', $id_usuario, $repetir");
+
+    if (($data >= date('Y-m-d')) && ($fim >= date('H:i:s')))    
+        Query::insert("tarefas", "nome, inicio, fim, data, id_usuario, id_recorrencia, ativo", "'$nome', '$inicio', '$fim', '$data', $id_usuario, $repetir, 1");
+
         
     return '../home.php';
 }
