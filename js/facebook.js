dcb14679fc22d71ec998408020b367955e12b79e
isaCarvalho <isabelacarvalhocf@hotmail.com>
2019-07-09 11:47:35 -0300

    Configurando login

--- js/facebook.js
+++ js/facebook.js
@@ -31,7 +31,7 @@ window.fbAsyncInit = function() {
     cookie     : true,  // enable cookies to allow the server to access 
                         // the session
     xfbml      : true,  // parse social plugins on this page
-    version    : '1.0' // The Graph API version to use for the call
+    version    : '3.3' // The Graph API version to use for the call
   });
 
   // Now that we've initialized the JavaScript SDK, we call 
@@ -66,8 +66,6 @@ window.fbAsyncInit = function() {
 function testAPI() {
   console.log('Welcome!  Fetching your information.... ');
   FB.api('/me', function(response) {
-    console.log('Successful login for: ' + response.name);
-    document.getElementById('status').innerHTML =
-      'Thanks for logging in, ' + response.name + '!';
+    console.log(response);
   });
 }
